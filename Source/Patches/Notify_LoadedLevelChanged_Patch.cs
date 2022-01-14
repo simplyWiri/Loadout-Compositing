ec962e5c7a5a0fc51c8344d71bcdbf49773b0146
simplyWiri <48577820+simplyWiri@users.noreply.github.com>
2022-01-14 22:37:20 +1100

    - Add ability to double click on a pawn and tab to them if you already have a Loadout Editor panel open - Implicitly supports: Owls Colonist Bar, Colony Groups, Colour Coded Mood bar - Move late mod patching into the Def loading patch

--- Source/Patches/Notify_LoadedLevelChanged_Patch.cs
+++ Source/Patches/Notify_LoadedLevelChanged_Patch.cs
@@ -1,12 +1,11 @@
 ﻿using System.Linq;
 using HarmonyLib;
-using RimWorld;
 using Verse;
 
 namespace Inventory {
 
     // triggers code to run after defs have been loaded.
-    [HarmonyPatch(typeof(Verse.Messages), nameof(Verse.Messages.Notify_LoadedLevelChanged))]
+    [HarmonyPatch(typeof(Messages), nameof(Messages.Notify_LoadedLevelChanged))]
     public class Notify_LoadedLevelChanged_Patch {
 
         private static bool patchedDefs = false;
@@ -24,6 +23,33 @@ namespace Inventory {
             Utility.CalculateDefLists();
 
             patchedDefs = true;
+
+            LateModPatches();
+        }
+
+        private static void LateModPatches() {
+
+            if (ModLister.HasActiveModWithName("RPG Style Inventory Revamped")) {
+                var method = AccessTools.Method("Sandy_Detailed_RPG_GearTab:FillTab");
+                if (method != null) {
+                    var hp = new HarmonyMethod(typeof(RPG_Inventory_Patch), nameof(RPG_Inventory_Patch.Transpiler));
+                    ModBase.harmony.Patch(method, transpiler: hp);
+                }
+            }
+
+            if (ModLister.HasActiveModWithName("[LTO] Colony Groups")) {
+                var handleClickMethod = AccessTools.Method("TacticalGroups.ColonistBarColonistDrawer:HandleClicks");
+                if (handleClickMethod != null) {
+                    var hp = new HarmonyMethod(typeof(ColonistBarClicker_Patch), nameof(ColonistBarClicker_Patch.Transpiler));
+                    ModBase.harmony.Patch(handleClickMethod, transpiler: hp);
+                }
+
+                var otherHandleClickMethod = AccessTools.Method("TacticalGroups.ManagementMenu:HandleClicks");
+                if (otherHandleClickMethod != null) {
+                    var hp = new HarmonyMethod(typeof(ColonistBarClicker_Patch), nameof(ColonistBarClicker_Patch.Transpiler));
+                    ModBase.harmony.Patch(otherHandleClickMethod, transpiler: hp);
+                }
+            }
         }
 
     }
