c7de054ec8872ed2e3d8e27a9169036545e25351
simplyWiri <finley.gooding@hotmail.com>
2023-07-20 23:04:24 +1000

    Fix bug wherein colonists that are tempoarily not with the colony can hold tags which have since been deleted

--- Source/Patches/Pawn_SetFaction.cs
+++ Source/Patches/Pawn_SetFaction.cs
@@ -9,7 +9,14 @@ namespace Inventory {
 
         public static void Postfix(Pawn __instance) {
             if (__instance.IsValidLoadoutHolder()) {
-                foreach (var tag in __instance.TryGetComp<LoadoutComponent>().Loadout.AllTags) {
+                var comp = __instance.TryGetComp<LoadoutComponent>();
+
+                // Remove any tags which may no longer exist, consider the case wherein a tag has been deleted
+                // while a colonist was arrested & left the faction. When joining again it will have invalid state
+                // because the entry was not properly cleaned up.
+                comp.Loadout.elements.RemoveAll(elem => elem.Tag is null);
+
+                foreach (var tag in comp.Loadout.AllTags) {
                     var list = LoadoutManager.PawnsWithTags[tag];
                     if (!list.Pawns.Contains(__instance)) {
                         list.Pawns.Add(__instance);
