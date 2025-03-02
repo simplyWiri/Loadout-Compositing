7404d641b853c4b8dd2cc3c732243001d8c9b70e
Luboš Luňák <l.lunak@centrum.cz>
2025-03-02 10:53:09 +0100

    improve 'Better Pawn Control' support
    Binding only the panic state to the alert button of BPC
    leads to an inconsistent experience - the primary functionality of BPC
    is to switch policies and the alert button merely enables/reverts a specific
    policy. It should be the same as activating the relevant policies manually,
    but that did not work.
    
    This patch makes BPC fully responsible for setting the loadout state when
    the alert button is activated, and also makes BPC handle properly set loadout
    states when BPC policy is changed. Requires support in BPC (submitted there).

---
+++ Source/Utility/BetterPawnControl.cs
@@ -0,0 +1,22 @@
+﻿using Verse;
+
+namespace Inventory {
+
+    // Support for Better Pawn Control mod to get/set an active loadout for a pawn.
+    public static class BetterPawnControl {
+
+        public static int GetLoadoutId(Pawn pawn) {
+            return pawn.GetActiveState()?.id ?? -1;
+        }
+
+        public static void SetLoadoutById(Pawn pawn, int id) {
+            foreach( LoadoutState state in LoadoutManager.States ) {
+                if( state.id == id) {
+                    pawn.SetActiveState(state);
+                    return;
+                }
+            }
+            pawn.SetActiveState(null);
+        }
+    }
+}
